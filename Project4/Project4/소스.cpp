e19f05e45af95ab746f6a8a048e1d1e3f10353ee
YunHojun <yhj020996@gmail.com>
2019-10-07 20:02:49 +0900

    pro4 update

--- Project4/Project4/소스.cpp
+++ Project4/Project4/소스.cpp
@@ -8,7 +8,13 @@ int main() {
 
 	cin >> a >> b >> c;
 
+
+
 	if (c == '/') {
+		if (b == 0 ) {
+			cout << "You can't input 0 to denominator. Now denominator is 0";
+			b = 1;
+		}
 		cout << a / b;
 	}
 }
