e9f6d8081d80207e83af810f51c7d47cbc56b62d
YunHojun <yhj020996@gmail.com>
2019-10-07 19:45:59 +0900

    divide p4

---
+++ Project4/Project4/소스.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	int b;
+	char c;
+
+	cin >> a >> b >> c;
+
+	if (b == 0) {
+		cout << "You can't input 0 to denominator. Now denominator is 0";
+		b = 1;
+	}
+
+	if (c == '/') {
+		cout << a / b;
+	}
+}
